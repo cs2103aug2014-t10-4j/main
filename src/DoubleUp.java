366518775699f5625220f7efca3ae732b640a2cd
Larry Low <delvinlowzy@gmail.com>
2014-09-20 19:16:33 +0800

    Add some stub methods for UI

--- src/DoubleUp.java
+++ src/DoubleUp.java
@@ -5,34 +5,49 @@ import java.io.File;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 
-
 public class DoubleUp{
-	private static Scanner scanner;
+	private static Scanner scanner = new Scanner(System.in);
 	private static final String MSG_WELCOME = "Welcome to DoubleUp!";
 	private static final String COMMAND_LINE = "Enter Command: ";
 	private static final String MSG_FAIL_READ_FILE = "Unable to read file.";
 	private static final String MSG_FAIL_ADD = "Unable to add line.";
 	private static final String MSG_MISSING_FILE = "File not found.";
-	
+
 	public static void main(String[] args){
 		String fileName = "DoubleUp.txt";
-		scanner = new Scanner(System.in);
 		File file= openFile(fileName);
-		
-		messageToUser(MSG_WELCOME);
+
+		messageToUser(createWelcomeMessage());
 		while(true){
-			System.out.print(COMMAND_LINE);
+			messageToUser(COMMAND_LINE);
 			String command = scanner.nextLine();
-			String result = executeCommand(command,file);
+			/*		
+			String[] command = parseCommand();
+			String operation = command[0];
+			//Create Task object.
+			Task taskToExecute = new Task(command);
+			String result = executeCommand(operation, taskToExecute, textFile);
+			 */
+
+			String result = executeCommand(command, file);
 			messageToUser(result);
 		}
-		
 	}
 	private static String executeCommand(String command, File file) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
+	//Concats the different messages to form the welcome message for the welcome screen
+	private static String createWelcomeMessage() {
+		//showToUser(getProgressBar);
+		//showToUser(getQOTD);
+		//showToUser(getGoal);
+		//showToUser(getHelpMessage);
+		return MSG_WELCOME;
+	}
+
+
 	public static void writeToFile(String lineToAdd, File file) {
 		BufferedWriter fileWritten;
 		try {
@@ -45,29 +60,29 @@ public class DoubleUp{
 		} catch (IOException e) {
 			messageToUser(MSG_FAIL_ADD);
 		}
-		
+
 	}
-	
+
 	// The function below serves to count the number of lines of text present in the file.
 	public static int numberOfLine(File file) {
 		Scanner input;
 		int lineNum = 0;
 		try {
 			input = new Scanner(file);
-		
+
 			if(!input.hasNext()){
 				input.close();
 				return lineNum;	
 			}
-			
+
 			else{
-				
+
 				while(input.hasNext()){
 					input.nextLine();
 					lineNum++;
 				}
 				input.close();	
-				}
+			}
 		} catch (FileNotFoundException e) {
 			messageToUser(MSG_MISSING_FILE);
 		}
@@ -86,8 +101,8 @@ public class DoubleUp{
 		}
 		return file;
 	}
-		public static void messageToUser(String text){
-			System.out.println(text);
-			
-		}
+	public static void messageToUser(String text){
+		System.out.println(text);
+
 	}
+}
