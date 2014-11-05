9ed5f9342956bd78070a00526302f6c8cc97b403
wuxiaoling <wuxiaoling02@gmail.com>
2014-11-05 23:25:51 +0800

    updated LogicTest.java class

--- src/LogicTest.java
+++ src/LogicTest.java
@@ -2,13 +2,113 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
-
 import java.io.File;
 import java.util.ArrayList;
-
-
-
 public class LogicTest {
+	
+	private static final String MSG_CLEARED_FILE = "List is cleared";
+	private static final String MSG_SORT_SUCCESS = "Successfully sorted by alphabetical order.";
+	private static final String MSG_SORT_SUCCESS_1 = "Successfully sorted by importance level.";
+	
+	String[] splitTask4 = {null,"testing", "ft", null, null,null, null, null , null};
+	Task task = new Task(splitTask4);
+	
+	String[] splitTask1 = {null,"testing task","22/12/2014","0000","null", "some many works need to do", "1", 
+			null , null};
+	Task task1 = new Task(splitTask1);	
+	
+	String[] splitTask2 = {null,"apple","ft",null, null, null,"2", null, null};
+	Task task2 = new Task(splitTask2);
+
+	String[] splitTask3 = {null,"banana", "ft", null,null, null,"3", null, null};
+	Task task3 = new Task(splitTask3);
+	
+	public File testFile = new File("testingStorage.txt");
+	public File testArchive = new File("testingArchive.txt");
+	
+	private static ArrayList<Task> List = new ArrayList<Task>();
+	private static ArrayList<Task> List1 = new ArrayList<Task>();
+
+	@Test
+	public void Test_clearContent() {
+		String expected = String.format(MSG_CLEARED_FILE, "");
+		String result = Logic.clearContent(testFile, testArchive);
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void Test_add() {
+		assertEquals("Added to testingStorage.txt: \"testing task\". Type .u to undo.",
+				Logic.add("add", task1, testFile));
+	}
+
+	@Test
+	/* This is a boundary case for the ArrayList task size partition */
+	public void Test_DeleteTask() {
+		Logic.add("add", task1, testFile);
+		Logic.add("add",task2,testFile);
+		/*test delete first valid index number*/
+		assertEquals("deleted \'1. testing task\'",
+				Logic.delete("delete", 1, task1, testFile, testArchive));
+		/*test delete invalid cases*/
+		//assertEquals("Unable to delete line",
+			//	Logic.delete("delete", -1, task1, testFile, testArchive));
+		//assertEquals("Unable to delete line",
+				//Logic.delete("delete", 3, task1, testFile, testArchive));
+		/*test delete a upper boundary case*/
+		assertEquals("deleted \'1. apple\'",
+				Logic.delete("delete", 2, task1, testFile, testArchive));
+	}
+	
+	@Test
+	public void Test_search() {
+		String[] splitTask = {null,"testing",null,null, null, null, null, null , null};
+		Task task = new Task(splitTask);
+		String[] splitTask5 = {null,null,null, null, null, null, null, null , null};
+		Task task5 = new Task(splitTask5);	
+		Logic.clearContent(testFile,testArchive);
+		Logic.add("add", task1, testFile);		
+		List.add(task1);
+		assertEquals(List, Logic.search(task));
+		Logic.clearContent(testFile, testArchive);
+		List1.clear();
+		List1.add(task5);
+		assertEquals("", Logic.search(task5));
+		Logic.clearContent(testFile, testArchive);
+	}
+
+	@Test
+	public void Test_sortByAlphabet() {
+		List1.clear();
+		List1.add(task1);
+		List1.add(task2);
+		List.add(task3);
+		Logic.add("add", task1, testFile);	
+		Logic.add("add", task2, testFile);	
+		Logic.add("add", task3, testFile);	
+		String expected = String.format(MSG_SORT_SUCCESS);
+		assertEquals(expected, Logic.sortByAlphabet(List1));
+		List1.clear();
+		Logic.clearContent(testFile, testArchive);
+		/*nothing to sort*/
+		assertEquals(Constants.MSG_NO_TASKS_TO_SORT, Logic.sortByImportance(List1));	
+	}
+	@Test
+	public void Test_sortByImportance(){
+		List1.clear();
+		List1.add(task1);
+		List1.add(task2);
+		List.add(task3);
+		Logic.add("add", task1, testFile);	
+		Logic.add("add", task2, testFile);	
+		Logic.add("add", task3, testFile);	
+		String expected = String.format(MSG_SORT_SUCCESS_1);
+		assertEquals(expected, Logic.sortByImportance(List1));
+		List1.clear();
+		/*nothing to sort*/
+		Logic.clearContent(testFile, testArchive);	
+		assertEquals(Constants.MSG_NO_TASKS_TO_SORT, Logic.sortByImportance(List1));	
+	}
 	/*
 	this.name = splitTask[1];
 	this.date = splitTask[2];
@@ -33,8 +133,8 @@ public class LogicTest {
 	
 	
 
-		public File testFile = new File("testingStorage.txt");
-		public File testArchive = new File("testingArchive.txt");
+		//public File testFile = new File("testingStorage.txt");
+		//public File testArchive = new File("testingArchive.txt");
 		
 		String taskInputFirst;
 		String taskInputName;;
@@ -361,87 +461,5 @@ public class LogicTest {
 		Logic.undo(testFile, testArchive);
 		
 	}
-	
-		private static final String MSG_CLEARED_FILE = "List is cleared";
-		private static final String MSG_SORT_SUCCESS = "Successfully sorted by alphabetical order.";
-		private static final String MSG_SORT_SUCCESS_1 = "Successfully sorted by importance level.";
-		
-		String[] splitTask4 = {null,"testing", "ft", null, null, null, null , null};
-		Task task = new Task(splitTask4);
-		String[] splitTask1 = {null,"testing task","22/10/2014","0000", "some many works need to do", "1", 
-				null , null};
-		Task task1 = new Task(splitTask1);
-		
-		String[] splitTask2 = {null,"apple","ft", null, null,"2", null, null};
-		Task task2 = new Task(splitTask2);
-
-		String[] splitTask3 = {null,"banana", "ft", null, null,"3", null, null};
-		Task task3 = new Task(splitTask3);
-		
-		//File testFile = new File("test.txt");
-		//File testArchive = new File("testingArchive.txt");
-		
-		private static ArrayList<Task> List = new ArrayList<Task>();
-		private static ArrayList<Task> List1 = new ArrayList<Task>();
-
-		@Test
-		public void Test_clearCommand() {
-			String expected = String.format(MSG_CLEARED_FILE, "");
-			String result = Logic.clearContent(testFile, testArchive);
-			assertEquals(expected, result);
-		}
-
-		@Test
-		public void Test_add() {
-			assertEquals("Added to test.txt: \"testing task\". Type .u to undo.",
-					Logic.add("add", task1, testFile));
-		}
-
-		@Test
-		/* This is a boundary case for the ArrayList task size partition */
-		public void Test_DeleteTask() {
-			Logic.add("add", task1, testFile);
-			Logic.add("add",task2,testFile);
-			/*test delete first valid index number*/
-			assertEquals("deleted from your list: \"testing task\"",
-					Logic.delete("delete", 1, task1, testFile, testArchive));
-			/*test delete invalid cases*/
-			assertEquals("Unable to delete line",
-					Logic.delete("delete", -1, task1, testFile, testArchive));
-			//assertEquals("Unable to delete line",
-					//Logic.delete("delete", 3, task1, testFile, testArchive));
-			/*test delete a upper boundary case*/
-			assertEquals("deleted from your list: \"apple\"",
-					Logic.delete("delete", 2, task1, testFile, testArchive));
-		}
-		
-		@Test
-		public void Test_search() {
-			String[] splitTask = {null,"testing", null, null, null, null, null , null};
-			Task task = new Task(splitTask);
-			//String[] splitTask4 = {null,null, null, null, null, null, null , null};
-			//Task task4 = new Task(splitTask4);	
-			Logic.clearContent(testFile,testArchive);
-			Logic.add("add", task1, testFile);		
-			List.add(task1);
-			//List1.add(task4);
-			assertEquals(List, Logic.search(task));
-			//assertEquals(null, Logic.search(task4));
-		}
-
-		@Test
-		public void Test_sortByAlphabet() {
-			List1.clear();
-			List1.add(task1);
-			List1.add(task2);
-			String expected = String.format(MSG_SORT_SUCCESS);
-			assertEquals(expected, Logic.sortByAlphabet(List1));		
-		}
-		@Test
-		public void Test_sortByImportance(){
-			String expected = String.format(MSG_SORT_SUCCESS_1);
-			assertEquals(expected, Logic.sortByImportance(List1));	
-		}
-
 	}
 	
