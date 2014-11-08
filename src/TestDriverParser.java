bc38831049c1e39d9a81d88ed1f3e3c266b67420
Lakshmanan <rk_lakshmanan@hotmail.com>
2014-11-08 23:48:15 +0800

    tagging test driver

--- src/TestDriverParser.java
+++ src/TestDriverParser.java
@@ -1,21 +1,9 @@
+//@author A0110937J
 import java.util.Scanner;
 
-
+//The following class is a test driver for testing the parser class
 public class TestDriverParser {
 
-	/*
-	private static Scanner sc;
-	public static void main(String[] args){
-		
-		String input = new String("");
-		sc = new Scanner(System.in);
-		while(true){
-			input = sc.nextLine();
-			System.out.println(getParsedInput(input));
-		}
-		
-	}
-	*/
 	public static String getParsedInput(String input){
 		String[] result = Parser.parseInput(input);
 		String finalResult = new String("");
