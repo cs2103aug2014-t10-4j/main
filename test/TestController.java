fd1a24ae887a8c89caafb801e520293d33a33c5a
Delvin Low Zheng Yang <delvinlowzy@gmail.com>
2014-11-09 16:18:13 +0800

    Update TestController

--- test/TestController.java
+++ test/TestController.java
@@ -6,58 +6,46 @@ import org.junit.Test;
 
 
 public class TestController {
-	private static final String MSG_ADD_SUCCESS = "Added to %s: \"%s\". Type .u to undo.";
-	private static final String MSG_ADD_FAIL = "Unable to add line";
-	private static final String MSG_CNT_DELETE_ZERO = "Cannot delete index equal or below 0.";
-	private static final String MSG_ITEM_TO_DELETE_NOT_FOUND = "Item #%d is not found.";
-	private static final String MSG_DELETE_SUCCESS = "Deleted from your list: \"%s\".";
-	private static final String MSG_SORT_FAIL = "Sorting failed";
-	private static final String MSG_SORT_SUCCESS = "Successfully sorted by %s.";
-	private static final String MSG_NO_TASKS_TO_SORT = "Not enough tasks to sort";
-	private static final String MSG_CLEAR_FILE_SUCCESS = "List is cleared";
-	private static final String MSG_FOUND_N_ITEMS = "Found %d items.";
 	private static File testFile = new File("testFile.txt");
 	private static File archiveFile = new File("testArchive.txt");
 
 	@Test
 	public void testExecuteCommand() {
-		String[] splitCommand = {"A", "24/12/2014", "1900", "details", "3", 
-				null , null};
 		//Testing add
-		//Successful with command
+		//Successful with add command
 		ResultOfCommand results = Controller.executeCommand("add abc", testFile, archiveFile);
-		assertEquals(String.format(MSG_ADD_SUCCESS, "testFile.txt", "abc"), results.getFeedback());
-		//Successful without command
+		assertEquals(String.format(Constants.MSG_ADD_SUCCESS, "testFile.txt", "abc"), results.getFeedback());
+		//Successful without add command
 		results = Controller.executeCommand("wahaha", testFile, archiveFile);
-		assertEquals(String.format(MSG_ADD_SUCCESS, "testFile.txt", "wahaha"), results.getFeedback());
-		//Unsuccessful
-		//results = Controller.executeCommand("add ", testFile, archiveFile);
-		//assertEquals(String.format(Constants.EMPTY_TASKNAME_MSG, "testFile.txt", "abc"), results.getFeedback());
+		assertEquals(String.format(Constants.MSG_ADD_SUCCESS, "testFile.txt", "wahaha"), results.getFeedback());
+		//Unsuccessful add
+		results = Controller.executeCommand("add ", testFile, archiveFile);
+		assertEquals(String.format(Constants.ERROR_EMPTY_ITEM, "Task name"), results.getFeedback());
 		
 		//Testing delete 
 		//0 is a boundary case
 		results = Controller.executeCommand("delete 0", testFile, archiveFile);
-		assertEquals(MSG_CNT_DELETE_ZERO, results.getFeedback());
+		assertEquals(Constants.MSG_DELETE_FAIL + ". ", results.getFeedback());
 		//Delete index -100 
 		results = Controller.executeCommand("delete -100", testFile, archiveFile);
-		assertEquals(MSG_CNT_DELETE_ZERO, results.getFeedback());
+		assertEquals(Constants.ERROR_INVALID_PARAMETER, results.getFeedback());
 		Controller.executeCommand("add abc", testFile, archiveFile);
 		Controller.executeCommand("add def", testFile, archiveFile);
 		Controller.executeCommand("add ghi", testFile, archiveFile);
 		//Delete index 10 when there are only three items.
 		results = Controller.executeCommand("delete 10", testFile, archiveFile);
-		assertEquals(String.format(MSG_ITEM_TO_DELETE_NOT_FOUND, 10), results.getFeedback());
+		assertEquals(Constants.MSG_DELETE_NON_EXISTENT.trim(), results.getFeedback());
 		//Delete index 5 - successful case
 		results = Controller.executeCommand("delete 5", testFile, archiveFile);
-		assertEquals(String.format(MSG_DELETE_SUCCESS, "ghi"), results.getFeedback());
+		assertEquals("Deleted '5. ghi' from your list.", results.getFeedback());
 		
 		//Testing sort
 		results = Controller.executeCommand("sort time", testFile, archiveFile);
-		assertEquals(String.format(MSG_SORT_SUCCESS, "date and time"), results.getFeedback());
+		assertEquals(String.format(Constants.MSG_SORT_SUCCESS, "date and time"), results.getFeedback());
 		results = Controller.executeCommand("sort alpha", testFile, archiveFile);
-		assertEquals(String.format(MSG_SORT_SUCCESS, "alphabetical order"), results.getFeedback());
+		assertEquals(String.format(Constants.MSG_SORT_SUCCESS, "alphabetical order"), results.getFeedback());
 		results = Controller.executeCommand("sort importance", testFile, archiveFile);
-		assertEquals(String.format(MSG_SORT_SUCCESS, "importance level"), results.getFeedback());
+		assertEquals(String.format(Constants.MSG_SORT_SUCCESS, "importance level"), results.getFeedback());
 		
 		//Testing search- boundary case: cannot find anything
 		results = Controller.executeCommand("search ghi", testFile, archiveFile);
@@ -67,10 +55,10 @@ public class TestController {
 		assertEquals(2, results.getListOfTasks().size());
 		//Testing delete all
 		results = Controller.executeCommand("delete all", testFile, archiveFile);
-		assertEquals(MSG_CLEAR_FILE_SUCCESS, results.getFeedback());
+		assertEquals(Constants.MSG_CLEARED_FILE, results.getFeedback());
 		//Boundary case - search empty list
 		results = Controller.executeCommand("search abc", testFile, archiveFile);
-		assertEquals(String.format(MSG_FOUND_N_ITEMS,0), results.getFeedback());
+		assertEquals(String.format(Constants.MSG_FOUND_N_ITEMS,0), results.getFeedback());
 	}
 
 }
